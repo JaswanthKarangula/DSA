382e6738f5d6a0aec42bd88bfd7a09b7025cfdd2
JaswanthKarangula <49749604+JaswanthKarangula@users.noreply.github.com>
2021-04-16 19:04:44 +0530

    Update Sherlock and subsets.cpp

--- Bitmask Dp/Sherlock and subsets.cpp
+++ Bitmask Dp/Sherlock and subsets.cpp
@@ -70,4 +70,4 @@ signed  main() {
     }
     return 0;
 }
-
+//https://www.hackerearth.com/practice/algorithms/dynamic-programming/bit-masking/practice-problems/algorithm/sherlock-and-coprime-subset/
